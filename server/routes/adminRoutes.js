573e9a5db1e1cda9d8ed501442076ab0804ec470
CJcode6754 <ceejayibabiosa@gmail.com>
2025-06-26 15:41:43 +0800

    feat: implement comments functionality with CRUD operations and integrate into blog and admin routes

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { adminLogin } from '../controllers/adminController.js';
+import { adminLogin, approvedCommentById, deleteCommentById, getAllComments, getDashboardData } from '../controllers/adminController.js';
+import auth from '../middleware/auth.js';
+import { getAllBlogs } from '../controllers/BlogController.js';
 
 const adminRouter = express.Router();
 
-adminRouter.post('/login', adminLogin)
+adminRouter.post('/login', adminLogin);
+adminRouter.get('/comments', auth, getAllComments);
+adminRouter.get('/blogs', auth, getAllBlogs);
+adminRouter.post('/delete-comment', auth, deleteCommentById);
+adminRouter.post('/approved-comment', auth, approvedCommentById);
+adminRouter.get('/dashboard', auth, getDashboardData);
 
 export default adminRouter;
