bdf57d5c58040993c2e859df629a3952dc8c5b3a
CJcode6754 <ceejayibabiosa@gmail.com>
2025-06-26 14:41:12 +0800

    feat: initialize server with Express, add admin and blog routes
    - Created package.json for server dependencies and scripts.
    - Implemented admin login route in adminRoutes.js.
    - Added blog post route with image upload in blogRoutes.js.
    - Set up server.js to connect to the database and configure middleware.

---
+++ server/config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+    try {
+        mongoose.connection.on('Connected', () => {
+            console.log("Database Connected")
+        })
+        await mongoose.connect(`${process.env.MONGODB_URI}/blog`)
+    } catch (error) {
+        console.log(error.message);
+    }
+}
+
+export default connectDB;
---
+++ server/controllers/BlogController.js
@@ -0,0 +1,57 @@
+import fs from "fs";
+import imagekit from "../config/imageKit.js";
+import Blog from "../models/Blog.js";
+export const addBlog = async (req, res) => {
+  try {
+    const { title, subTitle, description, category, isPublished } = JSON.parse(
+      req.body.blog
+    );
+
+    const imageFile = req.file;
+
+    // Check if all fields are present
+    if (!title || !description || !category || !imageFile) {
+      return res.json({ success: false, message: "Missing required fields" });
+    }
+
+    const fileBuffer = fs.readFileSync(imageFile.path);
+
+    // Upload Image to ImageKit
+    const response = await imagekit.upload({
+      file: fileBuffer,
+      fileName: imageFile.originalname,
+      folder: "/blogs",
+    });
+
+    // Optimize the Image
+    const optimizeImageUrl = imagekit.url({
+      path: response.filePath,
+      transformation: [
+        {
+          quality: "auto", //Auto Compression
+        },
+        {
+          format: "webp", // Convert to modern format
+        },
+        {
+          width: "1280", // Resize Image
+        },
+      ],
+    });
+
+    const image = optimizeImageUrl;
+
+    await Blog.create({
+      title,
+      subTitle,
+      description,
+      category,
+      image,
+      isPublished,
+    });
+
+    res.json({ success: true, message: "Blog added succesfully" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
